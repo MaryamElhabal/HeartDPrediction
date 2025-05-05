3e9075389327260701c283553fe85cb2e843e90f
Maryam Ali <mariam.aly.2024@aiu.edu.eg>
2025-05-05 20:09:21 +0300

    Add files via upload

--- MachineProject/BeatCheck.py
+++ MachineProject/BeatCheck.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (accuracy_score,confusion_matrix,ConfusionMatrixDisplay,f1_score,)
 import pickle 
 
 with open('RainForestModel.pkl', 'rb') as f:
